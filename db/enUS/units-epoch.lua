f207a27fe391de6068fae51a0d2de2d02a5f8205
Bennylavaa <Doctorbeefy@gmail.com>
2024-04-18 11:56:50 -0400

    fixed some stormwind npcs locs

--- db/enUS/units-epoch.lua
+++ db/enUS/units-epoch.lua
@@ -2,4 +2,7 @@ pfDB["units"]["enUS-epoch"] = {
   [14624] = "Master Smith Burninate",
   [46164] = "Grampy Stoutforge",
   [46163] = "Bhurind Stoutforge",
+  [14722] = "Clavicus Knavingham",
+  [2285] = "Count Remington Ridgewell",
+  [24729] = "Alicia",
 }
