d33f7f037efbdf2d6b2a050d41e5c9a3b035e7e9
Domen Skamlic <domen@skamlic.com>
2024-04-19 21:35:21 +0200

    removed merge conflict symbols

--- db/enUS/items-epoch.lua
+++ db/enUS/items-epoch.lua
@@ -3,11 +3,8 @@ pfDB["items"]["enUS-epoch"] = {
   [60388] = "Swiftpaw\'s Snout",
   [60018] = "Mine Spider Mandible",
   [60057] = "Mining Equipment",
-<<<<<<< HEAD
-  [60111] = "Case of Ore",
-=======
   [782] = "Painted Gnoll Armband",
   [60160] = "Blood Petal Leaf",
   [60162] = "Bloody Note",
->>>>>>> bb81ebfff0273597864d8c80dfb8414f50b5006a
+  [60111] = "Case of Ore",
 }
