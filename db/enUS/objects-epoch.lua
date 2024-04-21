0fe99ed91a32cd15f608090f3ef148280c360d8b
Bennylavaa <Doctorbeefy@gmail.com>
2024-04-21 13:25:36 -0400

    wanted sign

--- db/enUS/objects-epoch.lua
+++ db/enUS/objects-epoch.lua
@@ -10,4 +10,5 @@ pfDB["objects"]["enUS-epoch"] = {
   [4000008] = "Northshire Reed", -- Placeholder ID
   [4000009] = "Joseph\'s Watch", -- Placeholder ID
   [4000010] = "Joseph\'s Grave", -- Placeholder ID
+  [4000011] = "Wanted Sign", -- Placeholder ID
 }
