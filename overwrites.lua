93e5c3c2b21c98b7c2edff58db6a59d114ee2222
Bennylavaa <165105701+Bennylavaa@users.noreply.github.com>
2025-10-01 07:06:34 -0400

    Remove Anchorite Truuen TBC NPC

--- overwrites.lua
+++ overwrites.lua
@@ -71,6 +71,7 @@ do -- units/npcs
   pfDB["units"]["data-epoch"][17355] = { } -- Valusha
   pfDB["units"]["data-epoch"][17109] = { } -- Cersei Dusksinger
   pfDB["units"]["data-epoch"][17098] = { } -- Ambassador Dawnsinger
+  pfDB["units"]["data-epoch"][17238] = { } -- Anchorite Truuen
   
   -- Remove zg island npc 
   pfDB["units"]["data-epoch"][14903] = { } -- Al'tabim the All-Seeing
